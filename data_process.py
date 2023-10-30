568006b56605002ea641a65cfe339e7ee8fbc570
Lunzo <92251423+Lunzo822@users.noreply.github.com>
2023-10-30 16:52:32 +0800

    data_process.py

--- data_process.py
+++ data_process.py
@@ -20,7 +20,7 @@ def load_group(filepath, filenames):
         data = load_file(filepath + name)
         loaded.append(data)
 
-    # 按序列深度排序，按第三轴(转置)
+    # Sort by sequence depth, by third axis (transposed)
     loaded = dstack(loaded)
     return loaded
 
@@ -39,11 +39,11 @@ def load_dataset(suffix=""):
 
 
 def min_max_scale(test_data):
-    # Numpy会将该矩阵的后两维看成一个二维矩阵，h则代表w*c大小的矩阵的个数，整体矩阵是w*c矩阵在h维度上的堆叠
+    # Numpy will treat the last two dimensions of the matrix as a two-dimensional matrix, where h represents the number of matrices of w * c size. The overall matrix is a stack of w * c matrices on the h dimension
 
     maximums, minimums = test_data.max(axis=0).max(axis=0), test_data.min(axis=0).min(axis=0)
     test_data = test_data.astype("float32")
-    for j in range(test_data.shape[0]):  # shape求的是数据维数
+    for j in range(test_data.shape[0]):  # Shape calculates the dimensionality of the data
         for i in range(test_data.shape[2]):
             test_data[j, :, i] = (test_data[j, :, i] - minimums[i]) / (maximums[i] - minimums[i])
     return test_data
@@ -59,7 +59,7 @@ def test_min_max_scale(data):
     return data
 
 
-# 打乱数据和标签
+# Disrupting data and labels
 def shuffle(train_data, train_label):
     index = [i for i in range(len(train_data))]
     np.random.seed(5298)
@@ -70,28 +70,28 @@ def shuffle(train_data, train_label):
 
 
 def load_data(sigma):
-    # 导入数据
+    # import data
     data1, label1 = load_dataset("train/平上下楼")
     data2, label2 = load_dataset("train/上下坡")
 
     data_test, label_test = load_dataset("test/data_8滑窗")
 
-    # 垂直（按行）顺序堆叠数组
+    # Stack arrays in vertical (row by row) order
     data_train = np.vstack((data1, data2))
     label_train = np.vstack((label1, label2))
 
-    # # 加入噪声后的数据集
+    # # Dataset with added noise
     # data_train = gauss_noise_matrix(data_train, sigma)
     # data_test = gauss_noise_matrix(data_test, sigma)
 
-    # 数据归一化
+    # data normalization
     data_train = min_max_scale(data_train)
     data_train = data_train.transpose((0, 2, 1))
 
     data_test = test_min_max_scale(data_test)
-    data_test = data_test.transpose((0, 2, 1))      # 转置
+    data_test = data_test.transpose((0, 2, 1))      # transposition
 
-    # 打乱数据和标签
+    # Disrupting data and labels
     data_sh, label_sh = shuffle(data_train, label_train)
     data_shf, label_shf = shuffle(data_test, label_test)
 
